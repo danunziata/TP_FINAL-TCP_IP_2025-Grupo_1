f3f82fad60a2c4c2fe895e695a4becdd84d1a09d
Ponte-Matias <matiasponte20@gmail.com>
2025-06-03 11:41:38 -0300

    fix: solo 2 buckets, de 6 meses y 4 años

--- server/influxdb/init.sh
+++ server/influxdb/init.sh
@@ -12,21 +12,7 @@ ORG=power_logic
 TOKEN=token_telegraf
 
 # Crear buckets adicionales
-influx bucket create --name tension --retention 180d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-influx bucket create --name potencia_activa --retention 180d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-influx bucket create --name potencia_reactiva --retention 180d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-influx bucket create --name potencia_aparente --retention 180d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-influx bucket create --name factor_potencia --retention 180d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-influx bucket create --name demanda --retention 1460d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-
-# Crear labels
-# NO FUNCIONA A TRAVÉS DE Influx CLI
-#influx label create --name "potencia" --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-#influx label create --name "reactiva" --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
-
-# Asignar labels a buckets
-# NO FUNCIONA A TRAVÉS DE Influx CLI
-#influx bucket label add --bucket potencia_reactiva --label reactiva --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
+influx bucket create --name anualx4 --retention 1460d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
 
 # Crear secret
 influx secret update --key TOKEN_EXTRA --value otro_token --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
