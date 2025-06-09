09e1f958ee629176585a793c521ffe6a7f2283ef
Ponte-Matias <matiasponte20@gmail.com>
2025-06-09 16:44:18 -0300

    fix: agregado el tasks promedio de potencia

--- server/influxdb/init.sh
+++ server/influxdb/init.sh
@@ -13,6 +13,25 @@ TOKEN=token_telegraf
 
 # Crear buckets adicionales
 influx bucket create --name anualx4 --retention 1460d --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
+influx bucket create --name promedios --retention 0 --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
+
+# Crear Task que calcula promedios cada 15 min
+influx task create \
+  --org "$ORG" \
+  --host http://influxdb-influxdb-1:8086 \    # IMPORTANTE: es el nombre del servicio que aparece en el docker compose
+  --token "$TOKEN" <<EOF
+option task = {
+  name: "promedio_potencia_cada_15m",
+  every: 15m,
+  offset: 1m
+}
+
+from(bucket: "anualx4")
+  |> range(start: -15m)
+  |> filter(fn: (r) => r.tipo == "Potencia")
+  |> aggregateWindow(every: 15m, fn: mean, createEmpty: false)
+  |> to(bucket: "promedios", org: "$ORG")
+EOF
 
 # Crear secret
 influx secret update --key TOKEN_EXTRA --value otro_token --org "$ORG" --host "$INFLUX_URL" --token "$TOKEN"
